74338fde86a633b8a5a953e29d0439600aa10e26
mhmmedinan <minan.65@hotmail.com>
2024-07-15 23:19:57 +0300

    add generic message broker implementation

--- src/main/java/io/github/minan65/corepackage/messaging/kafka/consumer/KafkaConsumer.java
+++ src/main/java/io/github/minan65/corepackage/messaging/kafka/consumer/KafkaConsumer.java
@@ -2,6 +2,7 @@ package io.github.minan65.corepackage.messaging.kafka.consumer;
 
 import io.github.minan65.corepackage.abstractions.events.EventHandler;
 import io.github.minan65.corepackage.abstractions.events.IntegrationEvent;
+import io.github.minan65.corepackage.abstractions.messaging.transport.EventBusConsumer;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,16 +11,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class KafkaConsumer<TEvent extends IntegrationEvent> {
+public class KafkaConsumer<TEvent extends IntegrationEvent> implements EventBusConsumer<TEvent> {
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
 
-    private EventHandler<TEvent> eventHandler;
+    private EventHandler<IntegrationEvent> eventHandler;
 
     @KafkaListener(topics = "#{@eventLs}", groupId = "#{@eventGroupId}")
-    public void consume(TEvent event) {
+    public  <TEvent extends IntegrationEvent> void consume(TEvent event) {
 
         LOGGER.info(String.format("%s received => %s", event.getClass().getSimpleName(), event.toString()));
 
         eventHandler.handle(event);
     }
-}
+    }
